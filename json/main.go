dc60a8e6dd91e1c946f82f4449a6badc8e26a20f
Izabelly <izabellycristinemelo@gmail.com>
2026-02-22 21:24:12 -0300

    feat: json

--- json/main.go
+++ json/main.go
@@ -1,10 +1,13 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"os"
+)
 
 type Conta struct {
-	Numero int
-	Saldo  int
+	Numero int `json:"n"`
+	Saldo  int `json:"s" validade:"gt=0"`
 }
 
 func main() {
@@ -14,4 +17,17 @@ func main() {
 		panic(err)
 	}
 	println(string(res))
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.Encode(conta)
+
+	println("------------")
+	dados2 := []byte(`{"n":2323,"s":1000}`)
+	var conta2 Conta
+	err = json.Unmarshal(dados2, &conta2) // Unmarshal é a função que converte um JSON em struct
+	if err != nil {
+		panic(err)
+	}
+
+	println(conta2.Numero)
 }
