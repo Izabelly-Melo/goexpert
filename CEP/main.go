860bcd26cc40c712d5860823a787e47ad562b623
Izabelly <izabellycristinemelo@gmail.com>
2026-02-22 23:18:53 -0300

    feat: add search cep

--- CEP/main.go
+++ CEP/main.go
@@ -1,6 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
 
 func main() {
 	http.HandleFunc("/", SearchCepHandler)
@@ -20,8 +39,48 @@ func SearchCepHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	cepData, err := SearchCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	w.Write([]byte("OLA"))
+	/*
+		result, err := json.Marshal(cepData)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(result)
+	*/
+
+	json.NewEncoder(w).Encode(cepData)
+}
+
+func SearchCep(cep string) (*ViaCEP, error) {
+	res, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body := &ViaCEP{}
+	err = json.NewDecoder(res.Body).Decode(body)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		var dado ViaCEP
+		error := json.Unmarshal(body, &dado)
+		if error != nil {
+			return nil, error
+		}
+		return &dado, nil
+	*/
+
+	return body, nil
 }
