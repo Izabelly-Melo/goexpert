2a7666276a238d28ea1dbd6459acdbf280d8d566
Izabelly <izabellycristinemelo@gmail.com>
2026-02-25 23:23:50 -0300

    feat: aprendendo conceitos do context

---
+++ intro_contexts/hotel/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	fmt.Println("Starting hotel booking process...")
+	bookHotel(ctx)
+}
+
+func bookHotel(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("Hotel Booking cancelled. Timeout reached.")
+		return
+	case <-time.After(2 * time.Second):
+		fmt.Println("Hotel booked successfully.")
+	}
+}
