848d452c2c916e17d064a63e9eb627ec9cd27f47
Izabelly <izabellycristinemelo@gmail.com>
2026-03-08 00:49:35 -0300

    feat: primeiras consultas usando GORM

--- ORM/main.go
+++ ORM/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -20,11 +22,27 @@ func main() {
 
 	db.AutoMigrate(&Product{})
 
-	db.Create(&Product{Name: "Laptop", Price: 999.99})
+	//db.Create(&Product{Name: "Laptop", Price: 999.99})
 
-	products := []Product{
-		{Name: "Smartphone", Price: 499.99},
-		{Name: "Tablet", Price: 299.99},
+	//products := []Product{
+	//	{Name: "Smartphone", Price: 499.99},
+	//	{Name: "Tablet", Price: 299.99},
+	//}
+	//db.Create(&products)
+
+	p := Product{}
+	//db.First(&p, 1) // SELECT * FROM products WHERE id = 1
+	//fmt.Printf("Product: %+v\n", p)
+
+	db.First(&p, "name = ?", "Tablet") // SELECT * FROM products WHERE name = 'Laptop'
+	fmt.Printf("Product: %+v\n", p)
+
+	fmt.Println("_________________")
+
+	// select all products
+	products := []Product{}
+	db.Find(&products)
+	for _, p := range products {
+		fmt.Printf("Product: %+v\n", p)
 	}
-	db.Create(&products)
 }
