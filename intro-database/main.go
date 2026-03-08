11818253974b35bb8e979b0eadcaaaec88ec66e1
Izabelly <izabellycristinemelo@gmail.com>
2026-03-07 23:46:38 -0300

    feat: inserindo dados no db

--- intro-database/main.go
+++ intro-database/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Products struct {
 	ID    string
@@ -17,5 +22,29 @@ func NewProducts(name string, price float64) *Products {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
+	product := NewProducts("Notebook", 3000)
+	err = insertProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func insertProduct(db *sql.DB, p *Products) error {
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES(?, ?, ?)") // Prepara a query para inserção de um produto seguindo o padrão de segurança para evitar SQL Injection
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(p.ID, p.Name, p.Price)
+	if err != nil {
+		return err
+	}
+	return nil
 }
