53cc27e9fc2d066d8944acdc6736f81678af52b5
Izabelly <izabellycristinemelo@gmail.com>
2026-03-09 00:28:19 -0300

    feat: pegadinhas has many

--- has_many/main.go
+++ has_many/main.go
@@ -12,12 +12,19 @@ type Category struct {
 }
 
 type Product struct {
-	ID         int `gorm:"primaryKey"`
-	Name       string
-	Price      float64
-	CategoryID int      // chave estrangeira para Category
-	Category   Category // relação com Category
-	gorm.Model          // adiciona os campos CreatedAt, UpdatedAt e DeletedAt
+	ID           int `gorm:"primaryKey"`
+	Name         string
+	Price        float64
+	CategoryID   int      // chave estrangeira para Category
+	Category     Category // relação com Category
+	SerialNumber SerialNumber
+	gorm.Model   // adiciona os campos CreatedAt, UpdatedAt e DeletedAt
+}
+
+type SerialNumber struct {
+	ID        int `gorm:"primaryKey"`
+	Number    string
+	ProductID int // chave estrangeira para Product
 }
 
 func main() {
@@ -27,7 +34,7 @@ func main() {
 		panic("failed to connect database")
 	}
 
-	db.AutoMigrate(&Product{}, &Category{}) // cria a tabela products no banco de dados
+	db.AutoMigrate(&Product{}, &Category{}, &SerialNumber{}) // cria a tabela products no banco de dados
 
 	//db.Create(&Product{Name: "Laptop", Price: 999.99})
 	/*
@@ -46,15 +53,18 @@ func main() {
 		// INSERT INTO products (name, price, category_id) VALUES ('Smartphone', 499.99, 1)
 		db.Create(&Product{Name: "Smartphone", Price: 499.99, CategoryID: category.ID})
 
-		category := Category{Name: "Cozinha"}
+		db.Create(&SerialNumber{Number: "SN654321", ProductID: 1}) // INSERT INTO serial_numbers (number, product_id) VALUES ('SN654321', 1)
+
+		category = Category{Name: "Cozinha"}
 		db.Create(&category) // INSERT INTO categories (name) VALUES ('Cozinha')
 
 		// INSERT INTO products (name, price, category_id) VALUES ('Panela', 499.99, 1)
 		db.Create(&Product{Name: "Panela", Price: 499.99, CategoryID: category.ID})
-	*/
 
+		db.Create(&SerialNumber{Number: "SN654321", ProductID: 2}) // INSERT INTO serial_numbers (number, product_id) VALUES ('SN654321', 2)
+	*/
 	categories := []Category{}
-	err = db.Model(&Category{}).Preload("Products").Find(&categories).Error // Preload carrega os produtos associados a cada categoria
+	err = db.Model(&Category{}).Preload("Products.SerialNumber").Find(&categories).Error // Preload carrega os produtos e os números de série associados a cada categoria
 	if err != nil {
 		panic("failed to query categories")
 	}
@@ -62,7 +72,7 @@ func main() {
 	for _, category := range categories {
 		println(category.Name, ":")
 		for _, product := range category.Products {
-			println(" -", product.Name)
+			println(" -", product.Name, "- Serial Number:", product.SerialNumber.Number)
 		}
 	}
 
