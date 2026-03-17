531d206b4865932eaec3fbf5b87ff75737af9663
Izabelly <izabellycristinemelo@gmail.com>
2026-03-17 01:05:00 -0300

    feat: testing batch

---
+++ testing/tax_test.go
@@ -0,0 +1,34 @@
+package tax
+
+import (
+	"testing"
+)
+
+func TestCalculateTax(t *testing.T) {
+	amout := 500.0
+	expected := 5.0
+
+	result := CalculateTax(amout)
+	if result != expected {
+		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expected float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expected {
+			t.Errorf("Expected %f but got %f", item.expected, result)
+		}
+	}
+}
